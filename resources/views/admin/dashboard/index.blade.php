0c13bdf363b651d69cbca53aa2e7b06dfcf4ef64
Kadek Restu Satya Wardana <kadeksatya0399@gmail.com>
2023-01-06 14:26:33 +0800

    hide count

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,23 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <div class="col-md-6 col-lg-3">
-        <div class="card">
-            <div class="card-body">
-                <div class="media align-items-center">
-                    <div class="avatar avatar-icon avatar-lg avatar-blue">
-                        <i class="anticon anticon-coffee"></i>
-                    </div>
-                    <div class="m-l-15">
-                        <h2 class="m-b-0">{{$project}}</h2>
-                        <p class="m-b-0 text-muted">Upah Pekerja</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+
 <div class="row">
     <div class="col-md-6 col-lg-12">
         <div class="card">
